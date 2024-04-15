607ec7172d5e000a0222e2c745b8396f2d214907
Vanessa Viray <13virayvanessab@gmail.com>
2024-04-15 23:01:06 +0000

    Added an 'input' event listener to the Photo URL that sets the img src to the photo preview.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 'use strict';
-/* global data */
+// /* global data */
+const $input = document.querySelector('#photo-url-field');
+if (!$input) throw new Error('the photo-url-field query failed');
+function getPhotoUrl(event) {
+  const eventTarget = event.target;
+  const photoUrl = eventTarget.value;
+  const $image = document.querySelector('img');
+  if (!$image) throw new Error('the img query failed');
+  $image.src = photoUrl;
+}
+$input.addEventListener('input', getPhotoUrl);
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,14 @@
-/* global data */
+// /* global data */
+
+const $photoUrlElement = document.querySelector('#photo-url-field');
+if (!$photoUrlElement) throw new Error('the photo-url-field query failed');
+
+function getPhoto(event: any): void {
+  const eventTarget = event.target;
+  const photoUrl = eventTarget.value;
+  const $image = document.querySelector('img');
+  if (!$image) throw new Error('the img query failed');
+  $image.src = photoUrl;
+}
+
+$photoUrlElement.addEventListener('input', getPhoto);
