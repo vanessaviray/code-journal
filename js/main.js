b41e4ec4b649899c3bacb3b3c776a2d26f4615af
Vanessa Viray <13virayvanessab@gmail.com>
2024-04-17 04:26:31 +0000

    Created function renderEntry to generate and return dom tree.

--- js/main.js
+++ js/main.js
@@ -12,18 +12,44 @@ function getPhoto(event) {
 $photoUrlElement.addEventListener('input', getPhoto);
 const $entryForm = document.querySelector('#entry-form');
 if (!$entryForm) throw new Error('the entry-form query failed');
+const $formElements = $entryForm.elements;
+const $formData = {
+  title: $formElements.title.value,
+  photoUrl: $formElements.photoUrl.value,
+  notes: $formElements.notes.value,
+  entryId: data.nextEntryId,
+};
 $entryForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const $formElements = $entryForm.elements;
-  const $formData = {
-    title: $formElements.title.value,
-    photoUrl: $formElements.photoUrl.value,
-    notes: $formElements.notes.value,
-    entryId: data.nextEntryId,
-  };
   data.nextEntryId++;
   data.entries.unshift($formData);
   if (!$image) throw new Error('the img query failed');
   $image.src = 'images/placeholder-image-square.jpg';
   $entryForm.reset();
 });
+// generate and return a DOM tree for the li element
+function renderEntry(entry) {
+  const $liElement = document.createElement('li');
+  const $rowDiv = document.createElement('div');
+  $rowDiv.setAttribute('class', 'row');
+  $liElement.appendChild($rowDiv);
+  const $columnHalfDiv = document.createElement('div');
+  $columnHalfDiv.setAttribute('class', 'column-half');
+  $rowDiv.appendChild($columnHalfDiv);
+  const $columnHalfDiv2 = document.createElement('div');
+  $columnHalfDiv2.setAttribute('class', 'column-half');
+  $rowDiv.appendChild($columnHalfDiv2);
+  const $imgElement = document.createElement('img');
+  $imgElement.setAttribute('src', entry.photoUrl);
+  $columnHalfDiv.appendChild($imgElement);
+  const $h2Element = document.createElement('h2');
+  $columnHalfDiv2.appendChild($h2Element);
+  const titleValue = entry.title;
+  $h2Element.textContent = titleValue;
+  const $pElement = document.createElement('p');
+  $columnHalfDiv2.appendChild($pElement);
+  const notesValue = entry.notes;
+  $pElement.textContent = notesValue;
+  return $liElement;
+}
+console.log(renderEntry(data.entries[0]));
